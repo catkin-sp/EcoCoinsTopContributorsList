472d4d11e643d22c68dd326c397ee25cda01843e
kuznets <rsergei@hot.ee>
2016-02-10 18:57:58 +0200

    CreateFeatureTask

---
+++ EcoCoinsTopContributorsList/EcoCoins.Tests/EcoCoins.Tests/ShowTopUsers.Feature.feature
@@ -0,0 +1,48 @@
+﻿Feature: Show Top Users
+	Show top 10 ecocoins holders
+
+Scenario: No ecocoins holders
+	Given we have on people holding ecocoins
+	When user opens 'Top 10' page
+	Then get 'not userd found'
+
+Scenario: Less than 10 ecocoins holders
+	Given we have less than 10 people holding ecocoins
+	When user opens 'Top 10' page
+	Then show all people who holding ecocoins
+		And sort by decending order
+
+Scenario Outline: 10 ecocoins holders
+	Given we have 10 people holding ecocoins
+	When user opens 'Top 10' page
+	Then show 10 <users> who holding ecocoins
+		And sort by decending order
+		Examples: 
+		| users |
+		| Andrei |
+		| Alex   |
+		| Sergei |
+		| Ivan   |
+		| Anna   |
+		| Julia  |
+		| Steve  |
+		| Dawn   |
+		| Sergei |
+		| Dave   |
+
+Scenario: Over 10 ecocoins holders
+	Given we have more than 10 people holding ecocoins
+	When user opens 'Top 10' page
+	Then get the first 10 people who have more ecocoins
+		And show 10 people who holding ecocoins
+		And sort by decending order
+
+Scenario: Several people have some amount of ecocoins
+	Given we have more than 10 people holding ecocoins
+		And two or more people have same amount of ecocoins
+		And people registered their eococins at different times
+	When user opens 'Top 10' page
+	Then get the people who have ecocoins
+		And sort by decending order
+		And if coint amount is equal for sereval people, sort these people by registration date
+		And show 10 first people
---
+++ EcoCoinsTopContributorsList/EcoCoins.Tests/EcoCoins.Tests/ShowTopUsersSteps.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace EcoCoins.Tests
+{
+    [Binding]
+    public class ShowTopUsersSteps
+    {
+        [Given(@"we have on people holding ecocoins")]
+        public void GivenWeHaveOnPeopleHoldingEcocoins()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"we have less than (.*) people holding ecocoins")]
+        public void GivenWeHaveLessThanPeopleHoldingEcocoins(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"we have (.*) people holding ecocoins")]
+        public void GivenWeHavePeopleHoldingEcocoins(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"we have more than (.*) people holding ecocoins")]
+        public void GivenWeHaveMoreThanPeopleHoldingEcocoins(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"two or more people have same amount of ecocoins")]
+        public void GivenTwoOrMorePeopleHaveSameAmountOfEcocoins()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"people registered their eococins at different times")]
+        public void GivenPeopleRegisteredTheirEococinsAtDifferentTimes()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"user opens '(.*)' page")]
+        public void WhenUserOpensPage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"get '(.*)'")]
+        public void ThenGet(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"show all people who holding ecocoins")]
+        public void ThenShowAllPeopleWhoHoldingEcocoins()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"sort by decending order")]
+        public void ThenSortByDecendingOrder()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"show (.*) Andrei who holding ecocoins")]
+        public void ThenShowAndreiWhoHoldingEcocoins(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"get the first (.*) people who have more ecocoins")]
+        public void ThenGetTheFirstPeopleWhoHaveMoreEcocoins(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"show (.*) people who holding ecocoins")]
+        public void ThenShowPeopleWhoHoldingEcocoins(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"get the people who have ecocoins")]
+        public void ThenGetThePeopleWhoHaveEcocoins()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"if coint amount is equal for sereval people, sort these people by registration date")]
+        public void ThenIfCointAmountIsEqualForSerevalPeopleSortThesePeopleByRegistrationDate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"show (.*) first people")]
+        public void ThenShowFirstPeople(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
