f5e83f74fae985b2d9c7a14e232a51201815051f
kuznets <rsergei@hot.ee>
2016-02-21 14:32:36 +0200

    Create Users class.

---
+++ EcoCoinsTopContributorsList/EcoCoinsTopContributorsList/Models/EcoCoinsTopContributorsListContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EcoCoinsTopContributorsList.Models
+{
+    public class EcoCoinsTopContributorsListContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public EcoCoinsTopContributorsListContext() : base("name=EcoCoinsTopContributorsListContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<EcoCoinsTopContributorsList.Models.Users> Users { get; set; }
+    }
+}
---
+++ EcoCoinsTopContributorsList/EcoCoinsTopContributorsList/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcoCoinsTopContributorsList.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string RegDate { get; set; }
+    }
+}
